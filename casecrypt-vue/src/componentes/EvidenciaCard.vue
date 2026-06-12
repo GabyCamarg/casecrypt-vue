66cfcd01a1f497f20de36eb15534ab73b29e5295
Gaby <1gabycamargo@gmail.com>
2026-06-11 21:58:32 -0300

    titulo

---
+++ casecrypt-vue/src/componentes/EvidenciaCard.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="card">
+        <h3>{{ evidencia.titulo }}</h3>
+        <button @click=" $emit ('ver-detalhes')">
+            Ver Detalhes
+        </button>
+    </div>  
+</template>
+
+<script setup>
+defineProps(['evidencia']
+);
+</script>
