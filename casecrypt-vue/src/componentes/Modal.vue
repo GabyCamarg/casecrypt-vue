f225587c7c41359bb9087f1322bddfd663c48b33
Gaby <1gabycamargo@gmail.com>
2026-06-11 22:00:28 -0300

    modal

---
+++ casecrypt-vue/src/componentes/Modal.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="overlay">
+        <div class="modal">
+            <slot />
+
+            <button @click="$emit('fechar')">
+                Fechar
+            </button>
+        </div>
+    </div>
+</template>
