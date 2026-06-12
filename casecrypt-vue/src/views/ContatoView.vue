323e1b0b21b12ec8a4d2d4e75349c1d1509a20b0
Gaby <1gabycamargo@gmail.com>
2026-06-11 21:31:42 -0300

    contato

---
+++ casecrypt-vue/src/views/ContatoView.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <h1> Contato </h1>
+
+        <form @submit.prevent="enviar">
+            <div>
+                <label> Nome: </label>
+                <input type="text" v-model="nome"/>
+            </div>
