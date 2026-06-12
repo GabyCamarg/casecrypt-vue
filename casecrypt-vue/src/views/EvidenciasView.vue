192c4036dee26ba5f95c992fe25b25e3b656c592
Gaby <1gabycamargo@gmail.com>
2026-06-11 22:16:53 -0300

    corrigido erros

--- casecrypt-vue/src/views/EvidenciasView.vue
+++ casecrypt-vue/src/views/EvidenciasView.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { ref, computed } from 'vue';
 import EvidenciaCard from './componentes/EvidenciaCard.vue'
-import Modal from '@/componentes/Modal.vue';
+import Modal from './componentes/Modal.vue'
 
 const filtro = ref('');
 const modalAberto = ref(false);
