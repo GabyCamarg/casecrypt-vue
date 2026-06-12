d28acb88ebd2e3fdba26e17358757ba804818135
Gaby <1gabycamargo@gmail.com>
2026-06-11 21:48:14 -0300

    lista

--- casecrypt-vue/src/views/EvidenciasView.vue
+++ casecrypt-vue/src/views/EvidenciasView.vue
@@ -1,3 +1,8 @@
+<script setup lang="ts">
+import EvidenciaCard from '@/componentes/EvidenciaCard.vue';
+
+</script>
+
 <template>
     <div class="container">
         <h1>Evidencias</h1>
@@ -6,5 +11,14 @@
         v-model="filtro"
         placeholder="Filtrar evidencias..."
         class="input"
+        />
+    </div>
+
+    <div class="grid">
+        <EvidenciaCard
+            v-for ="(item, index) in evidenciasFiltradas"
+            :key="index"
+            :evidencia="item"
+            @ver-detalhes="verDetalhes(item)"
         />
     </div>
